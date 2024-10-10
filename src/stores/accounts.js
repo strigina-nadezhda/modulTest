1769ea9392c27bbb30b683c850e2ca76e58c49b3
strigina-nadezhda <126505183+strigina-nadezhda@users.noreply.github.com>
2024-10-10 17:47:40 +0300

    Add files via upload

--- src/stores/accounts.js
+++ src/stores/accounts.js
@@ -7,9 +7,13 @@ export const useAccountStore = defineStore("account", {
   getters: {},
   actions: {
     async fetchAccountData() {
+      const controller = new AbortController();
+      const timeoutId = setTimeout(() => controller.abort(), 5000); // Устанавливаем таймаут в 5 секунд
+
       try {
         const response = await fetch(
-          "https://my-json-server.typicode.com/plushevy/demo/meetings"
+          "https://my-json-server.typicode.com/plushevy/demo/meetings",
+          { signal: controller.signal }
         );
         if (!response.ok) {
           throw new Error(`Ошибка HTTP: ${response.status}`);
@@ -17,7 +21,13 @@ export const useAccountStore = defineStore("account", {
         const data = await response.json();
         this.rows = data;
       } catch (error) {
-        console.error("Ошибка при получении данных:", error);
+        if (error.name === "AbortError") {
+          console.error("Запрос был прерван по таймауту.");
+        } else {
+          console.error("Ошибка при получении данных:", error);
+        }
+      } finally {
+        clearTimeout(timeoutId); // Очистить таймаут
       }
     },
   },
